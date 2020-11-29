be4ffdb97961291a4dc5cefcfbbb63a2102f5b7b
carlosdaniel99 <cdmnkg@gmail.com>
2020-11-28 23:05:56 -0300

    improves

--- app/src/main/java/br/com/alura/app_multiactivity/Activity2.java
+++ app/src/main/java/br/com/alura/app_multiactivity/Activity2.java
@@ -5,17 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
-
-    private EditText stname;
-    private EditText stmedia;
-    private EditText stsitu;
-
     private String nome;
     private double media;
     private String situacao;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +21,12 @@ public class Activity2 extends AppCompatActivity {
         media = getIntent().getDoubleExtra("media", media);
         situacao = getIntent().getStringExtra("situacao");
 
-        stname = findViewById(R.id.stname);
-        stmedia = findViewById(R.id.stmedia);
-        stsitu = findViewById(R.id.stsitu);
-
+        TextView stname = findViewById(R.id.stname);
+        TextView stmedia = findViewById(R.id.stmedia);
+        TextView stsitu = findViewById(R.id.stsitu);
 
         stname.setText(nome);
         stmedia.setText(String.valueOf(media));
         stsitu.setText(situacao);
-
-
-
-
-
-
-
     }
 }
--- app/src/main/java/br/com/alura/app_multiactivity/MainActivity.java
+++ app/src/main/java/br/com/alura/app_multiactivity/MainActivity.java
@@ -4,21 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    Aluno aluno;
-
-    private EditText stname;
-    private EditText stnota1;
-    private EditText stnota2;
-    private EditText stfreq;
-
     private String nome;
     private double nota1;
     private double nota2;
@@ -26,60 +16,50 @@ public class MainActivity extends AppCompatActivity {
     private double media;
     private String situacao;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    public void notascal(View view) {
+    public void verifyGrade(View view) {
+        EditText stname = findViewById(R.id.stname);
+        EditText stnota1 = findViewById(R.id.stnota1);
+        EditText stnota2 = findViewById(R.id.stnota2);
+        EditText stfreq = findViewById(R.id.stfreq);
 
+        nome = stname.getText().toString();
+        nota1 = Double.parseDouble(stnota1.getText().toString());
+        nota2 = Double.parseDouble(stnota2.getText().toString());
+        frequencia = Integer.parseInt(stfreq.getText().toString());
 
-    stname = findViewById(R.id.stname);
-    stnota1 = findViewById(R.id.stnota1);
-    stnota2 = findViewById(R.id.stnota2);
-    stfreq = findViewById(R.id.stfreq);
+        if (nota1 > 10 || nota2 > 10 || frequencia > 100) {
+            Toast msg = Toast.makeText(this, "As notas devem ser de 1 a 10 e a frequência de 1 a 100", Toast.LENGTH_LONG);
+            msg.show();
+            return;
+        }
 
+        media = (nota1 + nota2)/2;
+        situacao = calculateResult(frequencia, media);
 
+        Intent intent = new Intent(MainActivity.this, Activity2.class);
+        intent.putExtra("nome", nome);
+        intent.putExtra("media", media);
+        intent.putExtra("situacao", situacao);
 
-    nome = stname.getText().toString();
-    nota1 = Double.parseDouble(stnota1.getText().toString());
-    nota2 = Double.parseDouble(stnota2.getText().toString());
-    frequencia = Integer.parseInt(stfreq.getText().toString());
-
-    media = (nota1 + nota2)/2;
-
-    if (frequencia < 75)
-    {
-        situacao = "reprovado por falta";
-    } else
-    {
-        if (media < 4)
-        {
-            situacao = "reprovado por nota";
-        } else
-            {
-                if (media >= 4 && media <= 6.9)
-                {
-                    situacao = "final";
-                } else
-                    {
-                        situacao = "aprovado";
-                    }
-            }
-
+        startActivity(intent);
     }
 
-
-
-    Intent intent = new Intent(MainActivity.this, Activity2.class);
-    intent.putExtra("nome", nome);
-    intent.putExtra("media", media);
-    intent.putExtra("situacao", situacao);
-
-    startActivity(intent);
-
+    public String calculateResult(Integer frequencia, Double media) {
+        if (frequencia < 75) {
+            return "Reprovado por falta";
+        }
+        if (media < 4) {
+            return "Reprovado por nota";
+        }
+        if (media >= 4 && media <= 6.9) {
+            return "Em final";
+        }
+        return "Aprovado";
     }
 }
